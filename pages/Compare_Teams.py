916e7e5085a6db2baea663a7b1e80107f22d6b6e
Joseph Paturi <thecloudtechnologist@gmail.com>
2024-11-19 14:36:22 +0000

    add: points by position

--- pages/Compare_Teams.py
+++ pages/Compare_Teams.py
@@ -36,11 +36,15 @@ def get_name(pick):
     points = players_df.loc[players_df['id']== pick]['event_points'].iloc[-1]
     return [name,position,points]
 
+
 ####
 drop_list = df_results['entry_name']
 #
-your_team = st.selectbox("Select Team1",drop_list,key='yours')
-their_team = st.selectbox("Select Team2",drop_list)
+col1, col2 = st.columns(2)
+with col1:
+    your_team = st.selectbox("Select Team1",drop_list,key='yours')
+with col2:
+    their_team = st.selectbox("Select Team2",drop_list)
 ##
 your_team_id = df_results.loc[df_results['entry_name'] == your_team]['entry'].iloc[-1]
 their_team_id = df_results.loc[df_results['entry_name'] == their_team]['entry'].iloc[-1]
@@ -49,19 +53,20 @@ your_list = get_picks(your_team_id,gwplayed)
 opp_list = get_picks(their_team_id,gwplayed)
 
 def compare_teams(your_id,their_id,gw):
-    common = list(set(your_list).intersection(opp_list))
-    common_picks = []
-    for i in common:
-        new = get_name(i)
-        common_picks.append(new)
-    df_common = pd.DataFrame(common_picks, columns=["Name","Position","GW points"]).sort_values(by=['Position'])
-    st.title("Common Players")
-    st.dataframe(df_common,hide_index=True,use_container_width=False)
-    st.title("Differentials")
-###
     col1, col2 = st.columns(2)
     with col1:
-        st.markdown("Your Players")
+        
+        pbp = points_by_position(your_list)
+        st.plotly_chart(pbp,theme=None,use_container_width=False)
+        common = list(set(your_list).intersection(opp_list))
+        common_picks = []
+        for i in common:
+            new = get_name(i)
+            common_picks.append(new)
+        df_common = pd.DataFrame(common_picks, columns=["Name","Position","GW points"]).sort_values(by=['Position'])
+        st.header("Common Players")
+        st.dataframe(df_common,hide_index=True,use_container_width=False)
+        st.header("Differentials")
         yours = set(your_list).difference(opp_list)
         your_picks = []
         for i in yours:
@@ -71,7 +76,18 @@ def compare_teams(your_id,their_id,gw):
         st.dataframe(df_yours, hide_index=True,height=600,use_container_width=False)
 ####
     with col2:
-        st.markdown("Their Players")
+        
+        pbp = points_by_position(opp_list)
+        st.plotly_chart(pbp,theme=None,use_container_width=False)
+        common = list(set(your_list).intersection(opp_list))
+        common_picks = []
+        for i in common:
+            new = get_name(i)
+            common_picks.append(new)
+        df_common = pd.DataFrame(common_picks, columns=["Name","Position","GW points"]).sort_values(by=['Position'])
+        st.header("Common Players")
+        st.dataframe(df_common,hide_index=True,use_container_width=False)
+        st.header("Differentials")
         theirs = set(opp_list).difference(your_list)
         their_picks = []
         for i in theirs:
@@ -79,5 +95,16 @@ def compare_teams(your_id,their_id,gw):
             their_picks.append(new)
         df_theirs = pd.DataFrame(their_picks, columns=["Name","Position","GW points"]).sort_values(by=['Position'])
         st.dataframe(df_theirs,hide_index=True,height=600,use_container_width=False)
-
+def points_by_position(picks_list):
+    team_picks = []
+    for i in picks_list:
+        new = get_name(i)
+        team_picks.append(new)
+    df_team_picks = pd.DataFrame(team_picks, columns=["Name","Position","Points"]).groupby('Position',as_index=False).sum()
+    fig_pos_scores = px.pie(df_team_picks,values='Points',names='Position',hover_data=['Name'],color='Position',hole=0.2)
+    return fig_pos_scores
+     
+#
+#
+st.caption("* Additional captain points are not counted")
 compare_teams(your_team_id,their_team_id,gwplayed)
